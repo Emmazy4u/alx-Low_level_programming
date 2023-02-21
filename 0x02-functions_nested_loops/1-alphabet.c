b431404a7553edc2c2a4c1fdbc338365ad2796ce
emma-obelenge <obeleemmanuel@gmail.com>
2023-02-21 13:09:26 -0800

    alphabet updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,15 @@
 
 
 /**
-* print_alphabet - print all alphabet in lowercase
- */
+*print_alphabet - print all alphabet in lowercase
+*/
 
 void print_alphabet(void)
+
 {
+
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 	_putchar(letter);
