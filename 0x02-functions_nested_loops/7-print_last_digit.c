c0d4df96edc04aebd176631e72ef48daa3bf10e6
emma-obelenge <obeleemmanuel@gmail.com>
2023-02-22 09:49:00 -0800

    there are only 3 colors updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,20 +4,16 @@
  * @n: the int to extract the last digit from
  * Return: value of the last digit
  */
+
 int print_last_digit(int n)
+
 {
 	int a;	
-
-	if (n < 0)
-		n = -n;
-
 	a = n % 10;
-		
 	if (a < 0)
-		a = -a;
-	
+	{
+	a  = a * -1;
+	}
 	_putchar(a + '0');
-	
 	return (a);
-		
 }
