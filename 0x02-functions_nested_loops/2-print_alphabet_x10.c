73316941301874d62416f5fb56f52c4ed02be1c7
emma-obelenge <obeleemmanuel@gmail.com>
2023-02-22 08:49:05 -0800

    alphabet update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * followed by a new line
- */
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+* followed by a new line
+*/
+
 void print_alphabet_x10(void)
+
 {
+
 	char c;
 	int i = 0;
 
-	while (i <= 10)
+	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
 	{
