6720b61c5726c407dd79be5c36d436d7215a6f51
emma-obelenge <obeleemmanuel@gmail.com>
2023-03-13 04:49:09 -0700

    4-print_most_numbers.c finally updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0.
  */
-void print_most_numbers(void)
+int print_most_numbers(void)
 {
 	int c;
 
 	for (c = 48; c < 58; c++)
 	{
-		if (c != 50 && c != 58;)
+		if (c != 50 && c != 58)
 		{
 			/*if (c != 52)
 			  {
@@ -22,4 +22,5 @@ void print_most_numbers(void)
 		}
 	}
 	_putchar('\n');
+	return (0);
 }
