bb5e125b0c0290c691b7cbe0f7481d9afe972c78
emma-obelenge <obeleemmanuel@gmail.com>
2023-03-30 14:10:29 -0700

    pushed up to task 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,4 @@
-include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
@@ -11,20 +11,17 @@ include <stdlib.h>
  */
 list_t *add_node(list_t **head, const char *str)
 {
- list_t *new;
- unsigned int len = 0;
+	list_t *new;
+	unsigned int len = 0;
 
- while (str[len])
- len++;
-
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
-
- new->str = strdup(str);
- new->len = len;
- new->next = (*head);
- (*head) = new;
-
- return (*head);
+	while (str[len])
+		len++;
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+	return (*head);
 }
