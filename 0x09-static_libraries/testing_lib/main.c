25fc67ae2c4eda6f8676a2616fd0589bbf93c3f7
emma-obelenge <obeleemmanuel@gmail.com>
2023-04-07 22:58:22 -0700

    added _putchar to libmy.a library

---
+++ 0x09-static_libraries/testing_lib/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main - Entty point
+ *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be");
+	_puts("the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
