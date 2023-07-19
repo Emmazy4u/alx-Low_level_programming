23f2b2a70a9e4870373173fae1154c45bafcb4bd
emma-obelenge <obeleemmanuel@gmail.com>
2023-07-18 22:06:47 -0700

    updated the 3-alloc_gri file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,6 +14,8 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	buff = (int **) malloc(sizeof(int *) * height);
+	if (buff == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		buff[i] = (int *) malloc(sizeof(int) * width);
