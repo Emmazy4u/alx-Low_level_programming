4ae01a49d7bf57f0130035c802ad6cd0d642de2e
Emmazy4u <emmazyandroid@gmail.com>
2023-08-30 09:55:04 +0000

    Added a testing dir for personal practice

---
+++ 0x12-singly_linked_lists/testing/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * node - Node declaration for linked list
+ * @str: Input string for each node
+ * @len: Input lengthe for the linked list
+ * @next: pointer to the next node or null at end of list
+ */
+struct node
+{
+	char str;
+	int len;
+	struct node *next;
+};
+
+/**
+ * list_t - Typdef for the struct node
+ */
+typedef struct node list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
