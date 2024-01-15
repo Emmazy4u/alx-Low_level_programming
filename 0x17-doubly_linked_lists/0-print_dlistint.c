d410512a2c27776673d6c6448ba64d47570429fe
emma-obelenge <obeleemmanuel@gmail.com>
2024-01-14 22:23:09 -0800

    add 0-print_dlistint.c file for task 0 of the doubly linked lists project

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,27 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - function that prints all elemets of a list
+ * @h: list passed to the function as parameter
+ *
+ * Return: returns the number of nodes in the list "h"
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
-    if (h != NULL)
-    {
-        const dlistint_t *ptr;
+	size_t count = 0;
 
-        ptr = h;
+	if (h != NULL)
+	{
+		const dlistint_t *ptr;
 
-        while (ptr != NULL)
-        {
-            count++;
-            printf("%d\n", ptr->n);
-            ptr = ptr->next;
-        }
-        return(count);
-    }
-    return (count);
+		ptr = h;
+		while (ptr != NULL)
+		{
+			count++;
+			printf("%d\n", ptr->n);
+			ptr = ptr->next;
+		}
+		return (count);
+	}
+	return (count);
 }
